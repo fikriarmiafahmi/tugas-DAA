4e290a410427c15440c9c9ae68b8b409b5c06363
Fikri AF <46241787+fikriarmiafahmi@users.noreply.github.com>
2024-10-16 17:45:58 +0700

    Update DQL.sql

--- Tugas Pertemuan 7/File SQL/DQL.sql
+++ Tugas Pertemuan 7/File SQL/DQL.sql
@@ -1,8 +1,21 @@
+-- TAMPILKAN TABLE
 SELECT * FROM formulir_pendaftaran;
-SELECT *  FROM data_siswa;
-SELECT *  FROM data_calon_siswa;
+SELECT * FROM data_siswa;
+SELECT * FROM data_calon_siswa;
 SELECT * FROM data_pembayaran;
-SELECT *  FROM bank;
-SELECT *  FROM ketua_yayasan;
-SELECT *  FROM kepala_sekolah;
-SELECT *  FROM hasil_test;
+SELECT * FROM bank;
+SELECT * FROM ketua_yayasan;
+SELECT * FROM kepala_sekolah;
+SELECT * FROM hasil_test;
+
+-- SHOW
+SHOW CREATE DATABASE pendaftaran_ois; 
+SHOW TABLES;
+SHOW CREATE TABLE data_siswa;
+SHOW COLUMNS FROM data_siswa; 
+
+-- TAMPILKAN VIEW
+SELECT * FROM nama_and_status;
+
+-- MATERIALIZE VIEW
+SELECT * FROM mv_pendaftaran_hasil_test;
